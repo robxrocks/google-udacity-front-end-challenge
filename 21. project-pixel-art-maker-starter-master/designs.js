db243600c25d428b8d7195ea9100c0711a612a8a
rr-t3ch <33297934+rr-t3ch@users.noreply.github.com>
2018-01-04 15:01:30 +0000

    Update designs.js

--- 21. project-pixel-art-maker-starter-master/designs.js
+++ 21. project-pixel-art-maker-starter-master/designs.js
@@ -1,38 +1,37 @@
 // Select color input
 var color = '#000';
 $('#colorPicker').change(function() {
-	color = $(this).val();
+    color = $(this).val();
 });
 
 // Select size input
-	$( "#sizePicker" ).submit(function(evt) {
-		var gridHeight, gridWidth;
+$( "#sizePicker" ).submit(function(evt) {
+    var gridHeight, gridWidth;
+
+    evt.preventDefault();
 		
-		evt.preventDefault();
-		
-		gridHeight = $('#input_height').val();
-		gridWidth = $('#input_width').val();
-		makeGrid(gridHeight, gridWidth);
-	});
+    gridHeight = $('#input_height').val();
+    gridWidth = $('#input_width').val();
+    makeGrid(gridHeight, gridWidth);
+});
 
 function makeGrid(height, width) {
-	var oldCanvas = $('#pixel_canvas');
-	oldCanvas.remove();
+    var oldCanvas = $('#pixel_canvas');
+    oldCanvas.remove();
 	
-	$('h2').last().append('<table id="pixel_canvas"></table>');
-	var newCanvas = $('#pixel_canvas');
-	for (var row = 0; row < height; row++){
-		newCanvas.append("<tr></tr>");
-		var currentRow = $('tr').last();
+    $('h2').last().append('<table id="pixel_canvas"></table>');
+    var newCanvas = $('#pixel_canvas');
+    for (var row = 0; row < height; row++){
+        newCanvas.append("<tr></tr>");
+        var currentRow = $('tr').last();
 		
-		for (var col = 0; col < width; col++) {
-			currentRow.append("<td></td>");
-			var currentCell = $('td').last();
+        for (var col = 0; col < width; col++) {
+            currentRow.append("<td></td>");
+            var currentCell = $('td').last();
 			
-			currentCell.on( "click", function() {
-				$(this).attr( 'bgcolor', color);
-			});
-		}
-	}
+            currentCell.on( "click", function() {
+                $(this).attr( 'bgcolor', color);
+            });
+        }
+    }
 }
-
